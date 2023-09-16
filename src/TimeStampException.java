701df6b24385283e9d9824a10ad096be309e8031
Patrick Noonan <p.noonan2@universityofgalway.ie>
2023-09-16 12:11:56 +0100

    ommitting first java assignmoent

---
+++ src/TimeStampException.java
@@ -0,0 +1,14 @@
+//Patrick Noonan(20355426)
+public class TimeStampException extends Exception{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public TimeStampException(String message)
+	{
+		super(message);
+	}
+
+}
