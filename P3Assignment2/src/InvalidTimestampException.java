7d4a6d01be69a6ed094c5db841778190eed967cd
Patrick Noonan <98957050+patrick437@users.noreply.github.com>
2023-09-16 12:48:00 +0100

    Delete P3Assignment2 directory

--- P3Assignment2/src/InvalidTimestampException.java
+++
@@ -1,9 +0,0 @@
-//Patrick Noonan(20355426)
-
-public class InvalidTimestampException extends Exception {
-
-	public InvalidTimestampException(String message) {
-		super(message);
-	}
-
-}
--- P3Assignment2/src/InvalidUserException.java
+++
@@ -1,9 +0,0 @@
-//Patrick Noonan(20355426)
-public class InvalidUserException extends Exception {
-	
-	public InvalidUserException(String message)
-	{
-		super(message);
-	}
-
-}
